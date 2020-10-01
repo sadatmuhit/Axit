49aa2304efc34fd9309c79194565134c1e03455a
Abu Sadat Md. Muhit <66241544+sadatmuhit@users.noreply.github.com>
2020-10-02 01:46:59 +0600

    Delete globalMethods.js

--- vendor/skeletabs/src/api/globalMethods.js
+++
@@ -1,9 +0,0 @@
-import { setClassNames } from '../core/classNames';
-import { setDefaults } from '../core/options';
-
-// Methods that affects all Skeletabs instances.
-// @example: $.skeletabs.setDefaults(myDefaults);
-export default {
-  setClassNames,
-  setDefaults,
-};
