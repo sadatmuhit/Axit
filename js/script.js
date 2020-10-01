90768bbe0f0f1772c20591db7f270a8d9216abb4
Abu Sadat Md. Muhit <66241544+sadatmuhit@users.noreply.github.com>
2020-10-02 01:01:01 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    //onepagenav
+    $('#main_nav').onePageNav({
+        currentClass: 'current',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function () {
+            //I get fired when the animation is starting
+        },
+        end: function () {
+            //I get fired when the animation is ending
+        },
+        scrollChange: function ($currentListItem) {
+            //I get fired when you enter a section and I pass the list item of the section
+        }
+    });
+    // scroll
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 450) {
+            $("#main_nav").addClass("fixed-top");
+        } else {
+            $("#main_nav").removeClass("fixed-top");
+        }
+    });
+    //skeletab
+    $('#sktab').skeletabs({
+        animation: "fade-scale"
+    });
+    //owlcarousel
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        slideTransition: "linear",
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 20,
+            },
+            576: {
+                items: 2,
+                margin: 20,
+                autoplay: true,
+            },
+            992: {
+                items: 3,
+                margin: 50,
+                dots: false
+            }
+        }
+    });
+});
---
+++ vendor/skeletabs/docs/assets/script.js
@@ -0,0 +1,184 @@
+(function ($, history, location) {
+  // navigation
+  $('#page')
+    .on('skeletabs:init', function (_, skeletabs) {
+      if (skeletabs.$container.is('#page') && skeletabs.currentIndex === 0) {
+        $('#firstExampleInner').on('skeletabs:init', function () {
+          $(this).skeletabs('play');
+        });
+      }
+    })
+    .on('skeletabs:move', function (_, skeletabs) {
+      if (skeletabs.$container.is('#page') && skeletabs.currentIndex === 0) {
+        $('#firstExampleInner').skeletabs('pause');
+      }
+    })
+    .on('skeletabs:moved', function (_, skeletabs) {
+      if (skeletabs.$container.is('#page') && skeletabs.currentIndex === 0) {
+        $('#firstExampleInner').skeletabs('reload').skeletabs('play');
+      }
+    })
+    .skeletabs(
+      {
+        breakpoint: 0,
+        history: 'push',
+        keyboardTabs: 'vertical',
+        transitionDuration: 300
+      },
+      {
+        tabGroup: 'nav-group',
+        tabItem: 'nav-item',
+        tab: 'nav-link',
+        panelGroup: 'content',
+        panel: 'section',
+        init: 'page-init',
+        tabsMode: 'page-tabs',
+        accordionMode: 'page-accordion'
+      }
+    );
+  // Insure #firstExampleInner is initialized after #page
+  $('#firstExampleInner').skeletabs({ panelHeight: 'adaptive' });
+
+  // internal links: move to the section
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    var hash = $(this).attr('href');
+    pushState(hash);
+    $('#page').skeletabs('goTo', hash);
+    event.preventDefault();
+  });
+
+  // dropdown
+  (function () {
+    var expectedKeys = {
+      // Desktop keyboard
+      13: 'enter',
+      38: 'up',
+      40: 'down',
+      // Home/End
+      36: 'home',
+      35: 'end',
+      // WSAD keys
+      87: 'up',
+      83: 'down'
+    };
+    var cache = {};
+
+    $(document).on('click', '.has-dropdown', function (event) {
+      event.preventDefault();
+      // console.log('click', this);
+      cache.$trigger = $(this);
+      cache.$dropdown = cache.$trigger.next('.dropdown');
+      cache.$options = cache.$dropdown.find('[role="option"]');
+      cache.selectedIndex = cache.$options.index(
+        cache.$options.filter('[aria-selected="true"]')
+      );
+      this.blur(); // prevent duplicate call on enter
+      if (cache.$trigger.attr('aria-expanded')) {
+        close();
+      } else {
+        open();
+      }
+    });
+
+    function close() {
+      if (!cache.$trigger) {
+        return;
+      }
+      console.log('close');
+      cache.$trigger.removeAttr('aria-expanded');
+      cache.$dropdown
+        .removeClass('dropdown-expanded')
+        .off('click', '[role="option"]', handleOptionClick);
+      $(document).off('click', handleBlur).off('keydown', handleKeydown);
+      // flush
+      cache = {};
+    }
+
+    function open() {
+      if (!cache.$trigger) {
+        return;
+      }
+      console.log('open');
+      cache.$trigger.attr('aria-expanded', 'true');
+      cache.$dropdown
+        .addClass('dropdown-expanded')
+        .on('click', '[role="option"]', handleOptionClick);
+      $(document).on('click', handleBlur).on('keydown', handleKeydown);
+    }
+
+    function select(index) {
+      var $selectedOption;
+      if (!cache.$options) {
+        return;
+      }
+      if (index < 0 || cache.$options.length <= index) {
+        return;
+      }
+      $selectedOption = cache.$options.eq(index);
+      cache.$options.removeAttr('aria-selected');
+      cache.$dropdown.attr('aria-activedescendant', $selectedOption.attr('id'));
+      cache.$trigger.find('.button-text').text($selectedOption.text());
+      $selectedOption.attr('aria-selected', 'true');
+      cache.selectedIndex = index;
+    }
+
+    function selectBy(step) {
+      if (!cache.$options) {
+        return;
+      }
+      select(cache.selectedIndex + step);
+    }
+
+    function handleBlur(event) {
+      var exception;
+      if (!cache.$trigger) {
+        return;
+      }
+      exception = cache.$trigger.parent().get(0);
+      if (event.target === exception || exception.contains(event.target)) {
+        return;
+      }
+      close();
+    }
+
+    function handleKeydown(event) {
+      var keycode = event.which || event.keyCode;
+      var key = expectedKeys[keycode];
+      if (!key) {
+        return;
+      }
+      switch (key) {
+        case 'up':
+          selectBy(-1);
+          break;
+        case 'down':
+          selectBy(1);
+          break;
+        case 'home':
+          select(0);
+          break;
+        case 'end':
+          select(cache.$options.length - 1);
+          break;
+        case 'enter':
+          cache.$options.eq(cache.selectedIndex).trigger('click');
+          break;
+        default:
+          break;
+      }
+    }
+
+    function handleOptionClick() {
+      close();
+      // return true;
+    }
+  })();
+
+  function pushState(hash) {
+    if (history && history.pushState) {
+      history.pushState({ hash: hash }, null, hash);
+    } else {
+      location.hash = hash;
+    }
+  }
+})(window.jQuery, window.history, window.location);
